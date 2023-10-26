dc9d6eeff40fe6be43104779226f4cc2a4eafb7f
Belati <belatijagadbintangsyuhada@gmail.com>
2023-10-26 07:59:46 +0700

    Basic template

---
+++ bookmark/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path, include
+from main.views import show_main
+
+app_name = 'main'
+
+urlpatterns = [
+]
---
+++ tambah_buku/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TambahBukuConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'tambah_buku'
